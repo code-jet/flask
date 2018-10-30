07bac61058835b19b27214f1151e3a13144ff958
Jewel <jewelelizabeththomas@gmail.com>
2018-10-31 03:05:44 +0530

    template added

--- fmarklist.py
+++ fmarklist.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 @app.route('/result')
 def result():
  dict={'phy':50,'che':60,'maths':70}
- return render_template('hmarklist.html', result=dict)
+ return render_template('marklist.html', result=dict)
 if __name__ == '__main__':
  app.run(debug=True)
